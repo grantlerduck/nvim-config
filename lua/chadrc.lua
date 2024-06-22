a8d3cf7a106fe08a7135286d2a936aff05612ebb
grantlerduck <daniel.alker@outlook.de>
2024-06-22 16:00:57 +0200

    init

---
+++ lua/chadrc.lua
@@ -0,0 +1,22 @@
+-- This file  needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+
+---@type ChadrcConfig
+local M = {}
+
+local overrides = require("configs.overrides")
+
+M.ui = {
+	theme = "chadracula",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- M.plugins = "plugins"
+
+M.ui = overrides.ui
+
+return M
