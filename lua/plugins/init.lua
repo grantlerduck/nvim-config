29d8c0d2e05c682f82e92e1e1a88c77a727a0e3d
grantlerduck <daniel.alker@outlook.de>
2024-07-21 18:00:46 +0200

    update plugins and add autosafe.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -189,6 +189,15 @@ local plugins = {
   {
     "vimwiki/vimwiki",
   },
+  {
+    "okuuva/auto-save.nvim",
+    cmd = "ASToggle", -- optional for lazy loading on command
+    event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
 
   -- {
   -- "zbirenbaum/copilot.lua",
