78acf02164cc2c7459f6ec9ab19b23b7fe966c5b
grantlerduck <daniel.alker@outlook.de>
2024-08-09 19:04:04 +0200

    chore: add plantuml

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,4 +1,4 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
@@ -7,12 +7,12 @@ local M = {}
 -- local overrides = require("configs.overrides")
 
 M.ui = {
-	theme = "chadracula",
+  theme = "chadracula",
   theme_toggle = { "chadracula", "chadracula-evondev" },
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+  -- hl_override = {
+  -- 	Comment = { italic = true },
+  -- 	["@comment"] = { italic = true },
+  -- },
 }
 
 -- M.plugins = "plugins"
