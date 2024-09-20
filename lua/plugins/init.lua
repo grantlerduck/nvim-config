fa16d73c0300d33ece5eda9abd144745305e6543
grantlerduck <47271781+grantlerduck@users.noreply.github.com>
2024-09-20 16:06:34 +0200

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -103,28 +103,23 @@ local plugins = {
       require "configs.lsp"
     end, -- Override to setup mason-lspconfig
   },
-
   -- override plugin configs
   {
     "williamboman/mason.nvim",
     opts = overrides.mason,
   },
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = overrides.treesitter,
   },
-
   {
     "nvim-tree/nvim-tree.lua",
     opts = overrides.nvimtree,
   },
-
   {
     "nvim-telescope/telescope.nvim",
     opts = overrides.telescope,
   },
-
   -- add telescope-fzf-native
   {
     "telescope.nvim",
@@ -137,19 +132,15 @@ local plugins = {
       end,
     },
   },
-
   {
     "lewis6991/gitsigns.nvim",
     opts = overrides.gitsigns,
   },
-
   {
     "hrsh7th/nvim-cmp",
     opts = overrides.cmp,
   },
-
   -- Additional plugins
-
   -- escape using key combo (currently set to jk)
   {
     "max397574/better-escape.nvim",
@@ -158,14 +149,12 @@ local plugins = {
       require "configs.betterescape"
     end,
   },
-
   {
     "mfussenegger/nvim-dap",
     config = function()
       require "configs.dap"
     end,
   },
-
   {
     "rcarriga/nvim-dap-ui",
     config = function()
@@ -173,7 +162,6 @@ local plugins = {
     end,
     requires = { "mfussenegger/nvim-dap" },
   },
-
   {
     "theHamsta/nvim-dap-virtual-text",
     config = function()
@@ -181,17 +169,14 @@ local plugins = {
     end,
     requires = { "mfussenegger/nvim-dap" },
   },
-
   -- better bdelete, close buffers without closing windows
   {
     "ojroques/nvim-bufdel",
     lazy = false,
   },
-
   {
     "nvim-lua/plenary.nvim",
   },
-
   {
     "vimwiki/vimwiki",
   },
@@ -204,7 +189,6 @@ local plugins = {
       -- or just leave it empty :)
     },
   },
-
   -- {
   -- "zbirenbaum/copilot.lua",
   --event = "InsertEnter",
@@ -213,7 +197,6 @@ local plugins = {
   --end,
   --endabled = false,
   --},
-
   {
     "leoluz/nvim-dap-go",
     ft = "go",
@@ -224,22 +207,18 @@ local plugins = {
   },
   {
     "javiorfo/nvim-soil",
-
     -- Optional for puml syntax highlighting:
     dependencies = { "javiorfo/nvim-nyctophilia" },
-
     lazy = true,
     ft = "plantuml",
     opts = {
       -- If you want to change default configurations
-
       -- If you want to use Plant UML jar version instead of the install version
       puml_jar = "/opt/homebrew/bin/plantuml",
       -- If you want to customize the image showed when running this plugin
       image = {
         darkmode = false, -- Enable or disable darkmode
         format = "svg", -- Choose between png or svg
-
         -- This is a default implementation of using nsxiv to open the resultant image
         -- Edit the string to use your preferred app to open the image (as if it were a command line)
         -- Some examples:
@@ -250,6 +229,45 @@ local plugins = {
         end,
       },
     },
+  },
+  {
+    "maxandron/goplements.nvim",
+    ft = "go",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "nvim-java/nvim-java",
+    lazy = false,
+    dependencies = {
+      "nvim-java/lua-async-await",
+      "nvim-java/nvim-java-core",
+      "nvim-java/nvim-java-test",
+      "nvim-java/nvim-java-dap",
+      "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
+      "mfussenegger/nvim-dap",
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          registries = {
+            "github:nvim-java/mason-registry",
+            "github:mason-org/mason-registry",
+          },
+        },
+      },
+    },
+    config = function()
+      require("java").setup {}
+      require("lspconfig").jdtls.setup {
+        on_attach = require("nvchad.configs.lspconfig").on_attach,
+        capabilities = require("nvchad.configs.lspconfig").capabilities,
+        filetypes = { "java" },
+      }
+    end,
   },
   {
     "vhyrro/luarocks.nvim",
@@ -263,13 +281,11 @@ local plugins = {
       require("crates").setup()
     end,
   },
-
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
   --   enabled = false
   -- },
-
   -- All NvChad plugins are lazy-loaded by default
   -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
   -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
@@ -277,5 +293,4 @@ local plugins = {
   --   "mg979/vim-visual-multi",
   --   lazy = false,
 }
-
 return plugins
